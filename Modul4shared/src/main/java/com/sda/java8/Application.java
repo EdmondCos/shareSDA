ab536b05103bca56be35ee9f37731c0c148a1ce0
Mohammad Majdi <mohammad.majdi818.gmail.com>
2019-04-13 14:02:19 +0300

    node

---
+++ Modul4shared/src/main/java/com/sda/java8/Application.java
@@ -0,0 +1,10 @@
+package com.sda.java8;
+
+public class Application {
+    public static void main(String[] args) {
+        Node node = new Node(20);
+        Node node1 = new Node(40);
+        Node node2 = new Node(50);
+        
+    }
+}
---
+++ Modul4shared/src/main/java/com/sda/java8/Node.java
@@ -0,0 +1,9 @@
+package com.sda.java8;
+
+public class Node {
+    int e ;
+    public Node (int e){
+        this.e=e;
+
+    }
+}
