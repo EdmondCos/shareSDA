490681f477805e0630e26e96eacae645f7e949ae
Edmond <edmond_costin@yahoo.com>
2019-04-13 14:02:09 +0300

    v0.03

---
+++ Modul4shared/src/main/java/com/sda/java8/Application.java
@@ -0,0 +1,10 @@
+package com.sda.java8;
+
+public class Application {
+    public static void main(String[] args) {
+        Node nod1 = new Node(20);
+        Node nod2 = new Node(40);
+        Node nod3 = new Node(50);
+
+    }
+}
---
+++ Modul4shared/src/main/java/com/sda/java8/Node.java
@@ -0,0 +1,10 @@
+package com.sda.java8;
+
+public class Node {
+    private int i;
+
+    public Node (int i) {
+        this.i = i;
+    }
+
+}
