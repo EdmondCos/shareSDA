8f62556ed5eaf3ff2015c19b48578152f6c5a85c
Edmond <edmond_costin@yahoo.com>
2019-04-27 17:19:16 +0300

    Gross Calculator

--- Modul4shared/src/main/java/Salary/a.java
+++ Modul4shared/src/main/java/Salary/a.java
@@ -23,30 +23,228 @@ public class a {
     private JTextField procentCM;
     private JTextField procentWeek;
     private JTextField procentSarbatori;
-    private JCheckBox impozit;
+    private JCheckBox scutiri;
     private JTextField procentWeekend;
+    private JPanel Input;
+    private JPanel Output;
+    private JPanel Pensie;
+    private JTextField pilon1;
+    private JTextField pilon2;
+    private JPanel Rest;
+    private JTextField CASS;
+    private JTextField Impozit;
+    private JPanel Bonuri;
+    private JTextField nrBon;
+    private JTextField valBon;
 
     private String sNet;
     private String sBrut;
 
+    private String numarBon;
+    private String valoareBon;
+
+    private String CMzile;
+    private String cmProcent;
+
+    private boolean faraScutiri = true;
+
     public a() {
         brut.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
                 sNet = brut.getText();
-                double brut = Integer.valueOf(sNet)*0.585;
-                sNet = String.valueOf(Math.round(brut));
-                net.setText(sNet);
+                double brut = Double.valueOf(sNet);
+                numarBon = nrBon.getText();
+
+                //TODO: Valoarea in caz ca userul pune "," la zecimala
+                valoareBon = valBon.getText();
+                String[] z = valoareBon.split("");
+                for (int i = 0; i < z.length; i++) {
+                    if (z[i].equals(",")) {
+                        z[i] = ".";
+                    }
+                }
+                StringBuilder x = new StringBuilder();
+                for (int i = 0; i < z.length; i++) {
+                    x.append(z[i]);
+                }
+                valoareBon = x.toString();
+
+                //TODO: Calcul valoare totala a bonurilor
+                double nrBonuri = 0;
+                if (!numarBon.isEmpty()) {
+                    nrBonuri = Double.valueOf(numarBon);
+                }
+                double valBonuri = 0;
+                if (!valoareBon.isEmpty()) {
+                    valBonuri = Double.valueOf(valoareBon);
+                }
+                double bonuri = nrBonuri * valBonuri;
+
+                //TODO: Calcul estimativ CM
+                CMzile = zileCM.getText();
+                double cmZile = 0;
+                if (!CMzile.isEmpty()){
+                    cmZile = Double.valueOf(CMzile);
+                }
+                cmProcent = procentCM.getText();
+                double CMporcent = 1 - Double.valueOf(cmProcent)/100;
+                double brutZi = brut/21;
+                brut = brut - (brutZi*CMporcent*cmZile);
+
+                //TODO: Verificare deduceri personale
+                double brutTotal = brut + bonuri;
+                double deductibil = getDeductibil(brutTotal);
+
+                //TODO: Calcule taxe si asigurari
+                int pUnu = (int) Math.round(brut * 0.2125);
+                int pDoi = (int) Math.round(brut * 0.0375);
+                int cass = (int) Math.round(brut * 0.10);
+                int asigurari = pUnu + pDoi + cass;
+
+                int impozit = 0;
+                if (faraScutiri) {
+                    impozit = (int) (Math.round((brut - asigurari + bonuri - deductibil) * 0.1));
+                }
+
+                //TODO: Output
+                int salariuNet = (int) Math.round(brut - pUnu - pDoi - cass - impozit);
+
+                net.setText(String.valueOf(salariuNet));
+                pilon1.setText(String.valueOf(pUnu));
+                pilon2.setText(String.valueOf(pDoi));
+                CASS.setText(String.valueOf(cass));
+                Impozit.setText(String.valueOf(impozit));
+
             }
         });
 
         net.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                sBrut = net.getText();
-                double net = Integer.valueOf(sBrut)/0.585;
-                sBrut = String.valueOf(Math.round(net));
-                brut.setText(sBrut);
+            @Override
+            public void actionPerformed(ActionEvent f) {
+
             }
         });
+        nrBon.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        valBon.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        pilon1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        pilon2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        CASS.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        Impozit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        scutiri.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (scutiri.isSelected()) {
+                    faraScutiri = false;
+                } else {
+                    faraScutiri = true;
+                }
+            }
+        });
+    }
+
+    private double getDeductibil(double brutTotal) {
+        double deductibil = 0;
+        if (brutTotal >= 1 && brutTotal <= 1950) {
+            deductibil = 510;
+        } else if (brutTotal >= 1951 && brutTotal <= 2000) {
+            deductibil = 495;
+        } else if (brutTotal >= 2001 && brutTotal <= 2050) {
+            deductibil = 480;
+        } else if (brutTotal >= 2051 && brutTotal <= 2100) {
+            deductibil = 465;
+        } else if (brutTotal >= 2101 && brutTotal <= 2150) {
+            deductibil = 450;
+        } else if (brutTotal >= 2151 && brutTotal <= 2200) {
+            deductibil = 435;
+        } else if (brutTotal >= 2201 && brutTotal <= 2250) {
+            deductibil = 420;
+        } else if (brutTotal >= 2251 && brutTotal <= 2300) {
+            deductibil = 405;
+        } else if (brutTotal >= 2301 && brutTotal <= 2350) {
+            deductibil = 390;
+        } else if (brutTotal >= 2351 && brutTotal <= 2400) {
+            deductibil = 375;
+        } else if (brutTotal >= 2401 && brutTotal <= 2450) {
+            deductibil = 360;
+        } else if (brutTotal >= 2451 && brutTotal <= 2500) {
+            deductibil = 345;
+        } else if (brutTotal >= 2501 && brutTotal <= 2550) {
+            deductibil = 330;
+        } else if (brutTotal >= 2551 && brutTotal <= 2600) {
+            deductibil = 315;
+        } else if (brutTotal >= 2601 && brutTotal <= 2650) {
+            deductibil = 300;
+        } else if (brutTotal >= 2651 && brutTotal <= 2700) {
+            deductibil = 285;
+        } else if (brutTotal >= 2701 && brutTotal <= 2750) {
+            deductibil = 270;
+        } else if (brutTotal >= 2751 && brutTotal <= 2800) {
+            deductibil = 255;
+        } else if (brutTotal >= 2801 && brutTotal <= 2850) {
+            deductibil = 240;
+        } else if (brutTotal >= 2851 && brutTotal <= 2900) {
+            deductibil = 225;
+        } else if (brutTotal >= 2901 && brutTotal <= 2950) {
+            deductibil = 210;
+        } else if (brutTotal >= 2951 && brutTotal <= 3000) {
+            deductibil = 195;
+        } else if (brutTotal >= 3001 && brutTotal <= 3050) {
+            deductibil = 180;
+        } else if (brutTotal >= 3051 && brutTotal <= 3100) {
+            deductibil = 165;
+        } else if (brutTotal >= 3101 && brutTotal <= 3150) {
+            deductibil = 150;
+        } else if (brutTotal >= 3151 && brutTotal <= 3200) {
+            deductibil = 135;
+        } else if (brutTotal >= 3201 && brutTotal <= 3250) {
+            deductibil = 120;
+        } else if (brutTotal >= 3251 && brutTotal <= 3300) {
+            deductibil = 105;
+        } else if (brutTotal >= 3301 && brutTotal <= 3350) {
+            deductibil = 90;
+        } else if (brutTotal >= 3351 && brutTotal <= 3400) {
+            deductibil = 75;
+        } else if (brutTotal >= 3401 && brutTotal <= 3450) {
+            deductibil = 60;
+        } else if (brutTotal >= 3451 && brutTotal <= 3500) {
+            deductibil = 45;
+        } else if (brutTotal >= 3501 && brutTotal <= 3550) {
+            deductibil = 30;
+        } else if (brutTotal >= 3551 && brutTotal <= 3600) {
+            deductibil = 15;
+        }
+        return deductibil;
     }
 
     public static void main(String[] args) {
--- Modul4shared/src/main/java/Salary/salaryCalculator.java
+++ Modul4shared/src/main/java/Salary/salaryCalculator.java
@@ -8,8 +8,90 @@ import java.util.Scanner;
 
 public class salaryCalculator {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x = 2100;
+        double y = 0.25;
+        double z = 2;
+        double a = x/21;
+        x = x- (a*z*y);
+        System.out.println(x);
 
 //        stuff();
+    }
+
+    private static double getDeductibil(double brutTotal) {
+        double deductibil = 0;
+        if (brutTotal >= 1 && brutTotal <= 1950) {
+            deductibil = 510;
+        } else if (brutTotal >= 1951 && brutTotal <= 2000) {
+            deductibil = 495;
+        } else if (brutTotal >= 2001 && brutTotal <= 2050) {
+            deductibil = 480;
+        } else if (brutTotal >= 2051 && brutTotal <= 2100) {
+            deductibil = 465;
+        } else if (brutTotal >= 2101 && brutTotal <= 2150) {
+            deductibil = 450;
+        } else if (brutTotal >= 2151 && brutTotal <= 2200) {
+            deductibil = 435;
+        } else if (brutTotal >= 2201 && brutTotal <= 2250) {
+            deductibil = 420;
+        } else if (brutTotal >= 2251 && brutTotal <= 2300) {
+            deductibil = 405;
+        } else if (brutTotal >= 2301 && brutTotal <= 2350) {
+            deductibil = 390;
+        } else if (brutTotal >= 2351 && brutTotal <= 2400) {
+            deductibil = 375;
+        } else if (brutTotal >= 2401 && brutTotal <= 2450) {
+            deductibil = 360;
+        } else if (brutTotal >= 2451 && brutTotal <= 2500) {
+            deductibil = 345;
+        } else if (brutTotal >= 2501 && brutTotal <= 2550) {
+            deductibil = 330;
+        } else if (brutTotal >= 2551 && brutTotal <= 2600) {
+            deductibil = 315;
+        } else if (brutTotal >= 2601 && brutTotal <= 2650) {
+            deductibil = 300;
+        } else if (brutTotal >= 2651 && brutTotal <= 2700) {
+            deductibil = 285;
+        } else if (brutTotal >= 2701 && brutTotal <= 2750) {
+            deductibil = 270;
+        } else if (brutTotal >= 2751 && brutTotal <= 2800) {
+            deductibil = 255;
+        } else if (brutTotal >= 2801 && brutTotal <= 2850) {
+            deductibil = 240;
+        } else if (brutTotal >= 2851 && brutTotal <= 2900) {
+            deductibil = 225;
+        } else if (brutTotal >= 2901 && brutTotal <= 2950) {
+            deductibil = 210;
+        } else if (brutTotal >= 2951 && brutTotal <= 3000) {
+            deductibil = 195;
+        } else if (brutTotal >= 3001 && brutTotal <= 3050) {
+            deductibil = 180;
+        } else if (brutTotal >= 3051 && brutTotal <= 3100) {
+            deductibil = 165;
+        } else if (brutTotal >= 3101 && brutTotal <= 3150) {
+            deductibil = 150;
+        } else if (brutTotal >= 3151 && brutTotal <= 3200) {
+            deductibil = 135;
+        } else if (brutTotal >= 3201 && brutTotal <= 3250) {
+            deductibil = 120;
+        } else if (brutTotal >= 3251 && brutTotal <= 3300) {
+            deductibil = 105;
+        } else if (brutTotal >= 3301 && brutTotal <= 3350) {
+            deductibil = 90;
+        } else if (brutTotal >= 3351 && brutTotal <= 3400) {
+            deductibil = 75;
+        } else if (brutTotal >= 3401 && brutTotal <= 3450) {
+            deductibil = 60;
+        } else if (brutTotal >= 3451 && brutTotal <= 3500) {
+            deductibil = 45;
+        } else if (brutTotal >= 3501 && brutTotal <= 3550) {
+            deductibil = 30;
+        } else if (brutTotal >= 3551 && brutTotal <= 3600) {
+            deductibil = 15;
+        }
+        return deductibil;
     }
 
     private static void stuff() {
