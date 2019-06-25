af015d08a7235628d09f9ecfe318afa019384394
Edmond <edmond_costin@yahoo.com>
2019-06-25 19:04:46 +0300

    moved projects

--- CoffeeProject/src/main/java/com/sda/IMachineFunctions.java
+++
@@ -1,6 +0,0 @@
-package com.sda;
-
-public interface IMachineFunctions {
-    Coffee brew(ECoffeeType type);
-    void restock();
-}
--- concentration/src/main/java/com/sda/IPlayable.java
+++
@@ -1,5 +0,0 @@
-package com.sda;
-
-public interface IPlayable {
-    int[] getCoordonates();
-}
