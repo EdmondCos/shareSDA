f3537bfb7dd382a51d731cc983c88eec77eb096d
Edmond <edmond_costin@yahoo.com>
2019-09-07 16:10:20 +0300

    frontend

---
+++ HTML+CSS+JavaScript/javascript/main.js
@@ -0,0 +1,31 @@
+var string = "Alert!"
+var int = 69
+//alert(string)
+console.log(int)
+
+var obj = {
+buch_8:["Andrei", "Filip","Stefan","Diana","Amalia"],
+buch_9:["Elena", "Alex", "Razvan", "Mara", "Stefania", "Ion"],
+}
+
+
+console.log(obj.buch_9 + "\nlength:" + obj.buch_9.length)
+console.log(obj.buch_8)
+var buch8 = obj.buch_8.length
+var buch9 = obj.buch_9.length
+console.log("Total elemente: " + (buch8 + buch9))
+
+function add(a, b){
+return buch8 + buch9
+}
+console.log("Suma este " + add(buch8,buch9))
+
+var element = document.getElementById("test2")
+var clasa = document.getElementsByClassName("ex")
+
+console.log(element)
+console.log(clasa)
+
+function myAlert(a){
+alert(a)
+}
